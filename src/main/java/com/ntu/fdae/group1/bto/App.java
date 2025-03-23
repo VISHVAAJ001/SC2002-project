8b5b9f5fdc2a1ca980dd92df3b79e67d86a062c3
Tommy Teo <52971804+ISnackable@users.noreply.github.com>
2025-03-23 15:38:02 +0000

    inital commit

---
+++ src/main/java/com/ntu/fdae/group1/bto/App.java
@@ -0,0 +1,11 @@
+package com.ntu.fdae.group1.bto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/ntu/fdae/group1/bto/AppTest.java
@@ -0,0 +1,18 @@
+package com.ntu.fdae.group1.bto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
